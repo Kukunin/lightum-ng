f3337387656e9474a99b3004a27112b1e0e47565
Sergiy Kukunin <sergey.kukunin@gmail.com>
2012-11-02 17:04:27 +0200

    Fix connection issue on DBus

--- src/dbus.cpp
+++ src/dbus.cpp
@@ -1,7 +1,6 @@
 #include "dbus.h"
 #include "config.h"
 
-#include <cstdio>
 #include <iostream>
 
 using namespace Core;
@@ -15,19 +14,35 @@ DBus::DBus( GBusType type ) {
 	DBus(type,"","","");
 }
 DBus::DBus( GBusType type, const char * busName, const char * objectPath, const char * interface ) {
+	//Depreciated in glib 2.36
 	if ( !typeInited ) {
 		g_type_init();
 		typeInited = true;
 	}
 
-	conn = g_bus_get_sync(type, NULL, &error);
-	if ( g_dbus_connection_is_closed(conn) ) {
-		printError();
-	}
-
 	this->busName = busName;
 	this->objectPath = objectPath;
 	this->interface = interface;
+
+	//ToDo make DBus connection global. Init and Close in one place
+	//Init private DBus connection
+	gchar* address = g_dbus_address_get_for_bus_sync (type, NULL, &error);
+
+	if (address == NULL) {
+		printError();
+	} else {
+		conn = g_dbus_connection_new_for_address_sync (address,
+				(GDBusConnectionFlags)(G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT
+					| G_DBUS_CONNECTION_FLAGS_MESSAGE_BUS_CONNECTION),
+				NULL,
+				NULL,
+				&error);
+		g_free (address);
+
+		if ( conn == NULL || g_dbus_connection_is_closed(conn) ) {
+			printError();
+		}
+	}
 
 }
 DBus::~DBus() {
@@ -35,6 +50,7 @@ DBus::~DBus() {
 		if (!g_dbus_connection_close_sync(conn,NULL,&error) ) {
 			printError();
 		}
+		g_object_unref(conn);
 	}
 	if ( error != NULL ) {
 		g_error_free(error);
@@ -87,7 +103,11 @@ int DBus::getInt(const gchar *method_name) {
 }
 
 void DBus::printError() {
-	if ( Core::Config::getInstance()->verbose() ) {
-		fprintf(stderr, "%s\n", error->message);
+	if ( error != NULL ) {
+		if (Core::Config::getInstance()->verbose()) {
+			std::cerr << error->message << std::endl;
+		}
+		g_error_free(error);
+		error = NULL;
 	}
 }
